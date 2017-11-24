dd4a60d0a76f7e5d963c7fc70c47563b06e64e00
JOSEPH CHEGE <josephchege08@gmail.com>
2017-11-24 16:07:37 +0300

    added injection on User.lookup.service

--- src/app/user-lookup.service.ts
+++ src/app/user-lookup.service.ts
@@ -7,7 +7,46 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class UserLookupService {
 
+  private searchUsersEndPoint = "https://api.github.com/search/users?q=";
+  private getUserDetailsEndPoint = "https://api.github.com/users/";
 
-  constructor() { }
+  constructor(private http: Http) { }
+  getusersByUserName (name: String){
+    let url;
+    if (name){
+    url = `${this.searchUsersEndPoint}username:${name}`;
+    }
 
-}
+    return this.http.get(url)
+          .map(this.extractData)
+          .catch(this.handleError);
+      }
+
+      getDetailsByUserName(username: string) {
+        if (username) {
+          let url = `${this.getUserDetailsEndPoint}${username}`;
+          return this.http.get(url)
+            .map((res: Response) => res.json())
+            .catch(this.handleError);
+        }
+      }
+
+      private extractData(res: Response) {
+        let body = res.json();
+        return body.items || {};
+      }
+
+      private handleError(error: Response | any) {
+        // In a real world app, you might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+          const body = error.json() || '';
+          const err = body.error || JSON.stringify(body);
+          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+          errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+      }
+    }
--- src/app/user-lookup/user-lookup.component.ts
+++ src/app/user-lookup/user-lookup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLookupService } from '../user-lookup.service';
 
 @Component({
   selector: 'app-user-lookup',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class UserLookupComponent implements OnInit {
   name: string;
 
-  constructor() { }
+  results: any[] = []; // This will hold the data coming from the service
+  selected: boolean = false; // Flag to check if a user is clicked or not
+  selectedUser: any; // presently Selected user details
+  error_text: string = ""; // So called error reporing text to the end user
+
+  constructor(private searchService: UserLookupService) { }
   ngOnInit() {}
 
   search (name: string){
